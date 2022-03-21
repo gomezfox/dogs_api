04ca7a729b21967d55add178ad0858a510c362b4
Stephen Gomez-Fox <stephen@gomezfox.net>
2022-03-21 01:22:43 -0700

    adds the Breed model (updated)

--- dogs/models.py
+++ dogs/models.py
@@ -1,7 +1,10 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Dog(models.Model):
+
+    # fields
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     breed = models.ForeignKey('Breed', related_name='dogs', on_delete=models.CASCADE)
@@ -9,6 +12,23 @@ class Dog(models.Model):
     color = models.CharField(max_length=50)
     favoritefood = models.CharField(max_length=80)
     favoritetoy = models.CharField(max_length=80)
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Breed(models.Model):
+
+    # choices
+    SIZE_CHOICES = (('TINY', 'Tiny'), ('SMALL', 'Small'), ('MEDIUM', 'Medium'), ('LARGE', 'Large'),)
+
+    # fields
+    name = models.CharField(max_length=200)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES, default='MEDIUM',)
+    friendliness = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    trainability = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    sheddingamount = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    exerciseneeds = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     class Meta:
         ordering = ('name',)
