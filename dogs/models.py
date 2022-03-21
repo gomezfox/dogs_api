f649ebc2914dbe033b8f6f679f8d9d56f2fbad81
Stephen Gomez-Fox <stephen@gomezfox.net>
2022-03-21 00:47:43 -0700

    adds the Dog model

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    breed = models.ForeignKey('Breed', related_name='dogs', on_delete=models.CASCADE)
+    gender = models.CharField(max_length=20)
+    color = models.CharField(max_length=50)
+    favoritefood = models.CharField(max_length=80)
+    favoritetoy = models.CharField(max_length=80)
+
+    class Meta:
+        ordering = ('name',)
